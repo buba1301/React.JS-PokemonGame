8eb1ce2f9a22045889b3fd7167684a2513c5026d
Boris Muzalevskii <bmuza57@gmail.com>
2021-02-06 13:10:03 +0300

    Homework 6 task 1 is done

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -1,39 +1,55 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import pokemons from '../../pokemons';
 
 import PokemonCard from '../../components/PokemonCard';
 
 import s from './GamePage.module.css';
 
+import { database } from '../../service/firebase';
+
 const GamePage = () => {
-  const [pokemonsList, setPokemons] = useState(pokemons);
+  const [pokemonsList, setPokemons] = useState({});
+
+  useEffect(() => {
+    database.ref('pokemons').once('value', (snapshot) => {
+      console.log('Snapshot', snapshot.val());
+      setPokemons(snapshot.val());
+    });
+  }, []);
 
   const handleOpenCard = (currentId) => {
     setPokemons((prevState) => {
-      return prevState.map((item) => {
-        const { active, id } = item;
+      return Object.entries(prevState).reduce((acc, [key, pokemonValues]) => {
+        const { active, id } = pokemonValues;
 
-        return currentId === id ? { ...item, active: !active } : item;
-      });
+        const newPokemon =
+          currentId === id
+            ? { ...pokemonValues, active: !active }
+            : pokemonValues;
+
+        return { ...acc, [key]: newPokemon };
+      }, {});
     });
   };
 
   return (
     <div className={s.flex}>
-      {pokemonsList.map(({ type, values, name, img, id, active }) => {
-        return (
-          <PokemonCard
-            key={id}
-            type={type}
-            values={values}
-            name={name}
-            img={img}
-            id={id}
-            handleOpenCard={handleOpenCard}
-            active={active}
-          />
-        );
-      })}
+      {Object.entries(pokemonsList).map(
+        ([key, { type, values, name, img, id, active }]) => {
+          return (
+            <PokemonCard
+              key={key}
+              type={type}
+              values={values}
+              name={name}
+              img={img}
+              id={id}
+              handleOpenCard={handleOpenCard}
+              active={active}
+            />
+          );
+        }
+      )}
     </div>
   );
 };
