3e01df019ba76e6c0a1f7355f4ba6d0e8a4d5207
Boris Muzalevskii <bmuza57@gmail.com>
2021-02-26 19:29:41 +0300

    Add logo svg

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,8 @@
 import cn from 'classnames';
 import s from './NavBar.module.css';
 
-import { ReactComponent as LoginSVG } from '../../assets/login.svg';
+import { ReactComponent as LogoSvg } from '../../assets/Logo.svg';
+import { ReactComponent as LoginSvg } from '../../assets/login.svg';
 
 const NavBar = ({
   handleOpenCloseMenu,
@@ -19,10 +20,12 @@ const NavBar = ({
   return (
     <nav id={s.navbar} className={navClassNames}>
       <div className={s.navWrapper}>
-        <p className={s.brand}>LOGO</p>
+        <p className={s.brand}>
+          <LogoSvg />
+        </p>
         <div className={s.loginAndMenu}>
           <div className={s.loginWrap} onClick={onClickLogin}>
-            <LoginSVG />
+            <LoginSvg />
           </div>
           <div className={buttonClassNames} onClick={handleClick}>
             <span />
