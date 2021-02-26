aa1d61756b0f59611932ed85edf9026d10b02ffa
Boris Muzalevskii <bmuza57@gmail.com>
2021-02-26 19:36:07 +0300

    Add handleCLose on span

--- src/components/MenuHeader/index.js
+++ src/components/MenuHeader/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import Menu from '../Menu';
+import Modal from '../Modal';
 import NavBar from '../Navbar';
 
 const MenuHeader = ({ bgActive }) => {
@@ -29,6 +30,13 @@ const MenuHeader = ({ bgActive }) => {
         bgActive={bgActive}
         onClickLogin={handleClickLogin}
       />
+      <Modal
+        title='Login for game...'
+        isOpen={isOpenModal}
+        onClickClose={handleClickLogin}
+      >
+        It is logi from
+      </Modal>
     </>
   );
 };
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,8 +1,11 @@
 import cn from 'classnames';
 
-import s from './Modal.modulr.css';
+import s from './Modal.module.css';
 
-const Modal = ({ title, children, isOpen }) => {
+const Modal = ({ title, children, isOpen, onClickClose }) => {
+  const handleCloseModal = () => {
+    onClickClose && onClickClose();
+  };
   return (
     <div
       class={cn(s.root, {
@@ -12,7 +15,7 @@ const Modal = ({ title, children, isOpen }) => {
       <div class={s.modal}>
         <div class={s.head}>
           {title}
-          <span class={s.btnClose}></span>
+          <span class={s.btnClose} onClick={handleCloseModal}></span>
         </div>
         <div className={s.content}>{children}</div>
       </div>
